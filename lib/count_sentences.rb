7e6d94ae8bab26732a3e5bc4623e2bbe073067ad
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-04-08 12:17:12 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?('.')
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?('?')
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    # if self.split(/[.?!]/).count === 0
+    #   return 0
+    # else
+    #   return self.split(/[.?!]/).reject! { |s| s.empty? }.count
+    # end
+    self.split(/\? |\. |\! /).count
   end
+
 end
